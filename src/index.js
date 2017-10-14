6f29c57a7ab8db5330e6f8010c1b0061a1a7a6e1
Rob Clarke <leon@moringamutual.org>
2017-10-14 12:12:14 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import http from "http";
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+import config from './config';
+import routes from './routes';
+
+let app = express();
+
+let app.server = http:createServer(app);
+
+// Middleware
+
+// Passport Config
+
+// API Routes v1
+
+app.use('/v1', routes);
+
+app.server.listen(config.port);
+
+console.log(`Server up & Listening at :${server.address().port}`);
+
+export default app;
